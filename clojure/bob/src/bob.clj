d7fe03944e9f13647f6ca5f038be042094bde84f
Joey.Huang <kamidox@qq.com>
2016-05-07 01:28:54 +0800

    clojure bob yell-v2

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -9,10 +9,16 @@
 
 (defn- question? [msg] (str/ends-with? msg "?"))
 
+(defn- question-v2? [msg] (= \? (last msg)))
+
+(defn- yell-v2? [msg]
+  (let [letters (filter #(Character/isLetter %) msg)]
+    (and (not (empty? letters)) (every? #(Character/isUpperCase %) letters))))
+
 (defn bob/response-for [msg]
   (cond 
     (silence? msg) "Fine. Be that way!"
-    (yell? msg) "Whoa, chill out!"
+    (yell-v2? msg) "Whoa, chill out!"
     (question? msg) "Sure."
     :else "Whatever."))
 
@@ -22,14 +28,16 @@
     (if (= n 0) (func msg)
       (recur (dec n)))))
 
-(defn- question-v2? [msg] (= \? (last msg)))
-
 (defn do-performance-test
- "run (do-performance-test) in repl to compare performance." 
+  "run (do-performance-test) in repl to compare performance." 
   ([] (do-performance-test 1000000))
   ([n]
-   (do (print "question-v1? with str/ends-with?: ")
+   (do (print "question-v1?: ")
        (time (performance-test question? "hello?" n))
-       (print "question-v2?: with last: ")
-       (time (performance-test question-v2? "hello?" n)))))
+       (print "question-v2?: ")
+       (time (performance-test question-v2? "hello?" n))
+       (print "yell-v1?: ")
+       (time (performance-test yell? "HELLO?" n))
+       (print "yell-v2?: ")
+       (time (performance-test yell-v2? "HELLO?" n)))))
 
