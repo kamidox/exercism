d15e3a64233d041664f0e12c5843d850156c32d0
Joey.Huang <kamidox@qq.com>
2016-05-07 00:57:44 +0800

    clojure bob

---
+++ clojure/bob/src/bob.clj
@@ -0,0 +1,35 @@
+(ns bob
+  (:require [clojure.string :as str]))
+
+(defn- silence? [msg] (str/blank? msg))
+
+(defn- has-letter? [msg] (some #(Character/isLetter %) msg))
+
+(defn- yell? [msg] (and (= msg (str/upper-case msg)) (has-letter? msg)))
+
+(defn- question? [msg] (str/ends-with? msg "?"))
+
+(defn bob/response-for [msg]
+  (cond 
+    (silence? msg) "Fine. Be that way!"
+    (yell? msg) "Whoa, chill out!"
+    (question? msg) "Sure."
+    :else "Whatever."))
+
+; compare the performance between clojure.string/ends-with? and last.
+(defn- performance-test [func msg n]
+  (loop [n n]
+    (if (= n 0) (func msg)
+      (recur (dec n)))))
+
+(defn- question-v2? [msg] (= \? (last msg)))
+
+(defn do-performance-test
+ "run (do-performance-test) in repl to compare performance." 
+  ([] (do-performance-test 1000000))
+  ([n]
+   (do (print "question-v1? with str/ends-with?: ")
+       (time (performance-test question? "hello?" n))
+       (print "question-v2?: with last: ")
+       (time (performance-test question-v2? "hello?" n)))))
+
