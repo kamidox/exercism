de7b84da3123d66d944dab6302cefefe1e79f57e
Joey.Huang <kamidox@qq.com>
2016-05-10 21:12:45 +0800

    clojure phone number v2

--- clojure/phone-number/src/phone_number.clj
+++ clojure/phone-number/src/phone_number.clj
@@ -1,4 +1,5 @@
-(ns phone-number)
+(ns phone-number
+  (:require [clojure.string :as str]))
 
 (defn number [phn]
   (let [norm-phn (->> phn (re-seq #"\d+") (reduce str))]
@@ -15,3 +16,45 @@
     (str "(" (take-num (range 3)) ") " (take-num (range 3 6)) "-"
          (take-num (range 6 10)))))
 
+; =============================================================================
+; Official solution: extract-parts is beautiful
+; =============================================================================
+(defn- digits-only
+  [input]
+  (str/replace input #"\D" ""))
+
+(defn- extract-parts
+  [input]
+  (if-let [matches (re-find #"^1?(...)(...)(....)$" input)]
+    (rest matches)
+    ["000" "000" "0000"]))
+
+(defn- parts
+  [input]
+  (-> input
+      digits-only
+      extract-parts))
+
+(defn number-v2 [input]
+  (str/join (parts input)))
+
+(defn area-code [input]
+  (first (parts input)))
+
+(defn pretty-print [input]
+  (let [[area-code exchange subscriber] (parts input)]
+    (str "(" area-code ") " exchange "-" subscriber)))
+
+; ==========================================================================
+; Performance Test
+; ==========================================================================
+(defn do-performance-test
+  ([] (do-performance-test 10000))
+  ([n]
+   (println "================ performance test ================")
+   (print "number: ")
+   (time (dorun (repeatedly n #(number "112234567890"))))
+   (print "number-v2: ")
+   (time (dorun (repeatedly n #(number-v2 "112234567890"))))))
+
+; (do-performance-test)
