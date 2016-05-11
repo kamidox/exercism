06cf428382be5348bde0fae66bd387000547f2b9
Joey.Huang <kamidox@qq.com>
2016-05-12 00:01:38 +0800

    clojure binary search v2

--- clojure/binary-search/src/binary_search.clj
+++ clojure/binary-search/src/binary_search.clj
@@ -10,8 +10,33 @@
          mid-id (middle coll)]
     (cond (= target-val (nth coll mid-id)) mid-id
           (or (= start-id mid-id) (= mid-id end-id))
-          (throw (AssertionError. "not found"))
+          (throw (Exception. "not found"))
           (< target-val (nth coll mid-id))
           (recur start-id mid-id (int (/ (+ start-id mid-id) 2)))
           :else (recur mid-id end-id (int (/ (+ mid-id end-id) 2))))))
 
+; ======================================================================
+; Official solution
+; ======================================================================
+(defn search-for-v2
+  [elem alist]
+  (let [middle (middle alist)
+        cur-elem (nth alist middle)]
+    (cond
+      (= cur-elem elem) middle
+      (or (= middle (count alist)) (zero? middle)) (throw (Exception. (format "%s not found in list" elem)))
+      (< cur-elem elem) (+ middle (search-for-v2 elem (drop middle alist)))
+      (> cur-elem elem) (search-for-v2 elem (take middle alist)))))
+; ==========================================================================
+; Performance Test
+; ==========================================================================
+(defn do-performance-test
+  ([] (do-performance-test 1))
+  ([n]
+   (println "================ performance test ================")
+   (print "search-for: ")
+   (time (dorun (repeatedly n #(search-for 999999 (range 1000000)))))
+   (print "search-for-v2: ")
+   (time (dorun (repeatedly n #(search-for-v2 999999 (range 1000000)))))))
+
+; (do-performance-test)
