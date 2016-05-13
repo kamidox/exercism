70b099e632885bfe915cf03bc33909713723bb70
Joey.Huang <kamidox@qq.com>
2016-05-14 00:16:09 +0800

    clojure triangle v2

--- clojure/triangle/src/triangle.clj
+++ clojure/triangle/src/triangle.clj
@@ -12,3 +12,15 @@
         (valid-triangle? a b c) :scalene
         :else :illogical))
 
+; ======================================================================
+; Inspire by Official Solution: sorted before compare. count distinct.
+; ======================================================================
+
+(defn type [a b c]
+  (let [sorted (sort > [a b c])]
+    (if (>= (first sorted) (apply + (rest sorted))) :illogical
+        (case (count (distinct sorted))
+          1 :equilateral
+          2 :isosceles
+          3 :scalene))))
+
