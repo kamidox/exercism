50f3cd831774e34c40c81d3fdf9ea980f6df5c80
Joey.Huang <kamidox@qq.com>
2016-05-14 23:21:45 +0800

    clojure trinary v2

--- clojure/trinary/src/trinary.clj
+++ clojure/trinary/src/trinary.clj
@@ -23,3 +23,27 @@
          (map power)
          (apply +))))
 
+; ======================================================================
+; Officlal solution is shorter, and the trick is `.compareTo` java function.
+; ======================================================================
+(defn to-decimal-v2 [num]
+  (loop [sum 0, num num]
+    (if (empty? num)
+      sum
+      (let [diff (.compareTo (first num) \0)]
+        (if (<= 0 diff 2)
+          (recur (+ (* sum 3) diff) (next num))
+          0)))))
+; ==========================================================================
+; Performance Test
+; ==========================================================================
+(defn do-performance-test
+  ([] (do-performance-test 10000))
+  ([n]
+   (println "================ performance test ================")
+   (print "to-decimal: ")
+   (time (dorun (repeatedly n #(to-decimal "1021021102"))))
+   (print "to-decimal-v2: ")
+   (time (dorun (repeatedly n #(to-decimal-v2 "1021021102"))))))
+
+; (do-performance-test)
