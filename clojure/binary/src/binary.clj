5f24a76fed8c8b00380a9639689053f34a26e8fb
Joey.Huang <kamidox@qq.com>
2016-05-14 22:00:10 +0800

    clojure binary

---
+++ clojure/binary/src/binary.clj
@@ -0,0 +1,24 @@
+(ns binary)
+
+(defn- exp [x n] (reduce * (repeat n x)))
+
+(defn- to-int [b] (- (int b) (int \0)))
+
+(defn- to-decimal-bit [cnt idx bit]
+  "Convert binary bit to decimal bit. e.g. 100 -> 4; 1000 -> 8"
+  (* (to-int bit) (exp 2 (- cnt idx))))
+
+(defn- invalid-binary? [bin-str]
+  (let [invalids (filter #(let [v (to-int %)] (or (>= v 2) (< v 0))) bin-str)]
+       (not (empty? invalids))))
+
+(defn to-decimal
+  "Convert string present binary to decimal. e.g. '1001' -> 9"
+  [bin-str]
+  (if (invalid-binary? bin-str) 0
+    (let [cnt (count bin-str)]
+      (->> bin-str
+           ; "101" -> [4 0 1]
+           (map-indexed (partial to-decimal-bit (dec cnt)))
+           (reduce +)))))
+
