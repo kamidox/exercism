360414ba37af9e605b4c5572646e1a5912c09c89
Joey.Huang <kamidox@qq.com>
2016-05-18 22:42:51 +0800

    clojure sieve

---
+++ clojure/sieve/src/sieve.clj
@@ -0,0 +1,13 @@
+(ns sieve)
+
+(defn- prime-of? [n p]
+  (or (<= n p) (not (zero? (mod n p)))))
+
+(defn sieve
+  "Return prime seq from 2 to n inclusive. e.g. 9 -> [2 3 5 7]"
+  [n]
+  (loop [primes (range 2 (inc n)) idx 0]
+    (if (<= (count primes) idx) primes
+      (let [p (nth primes idx)]
+        (recur (filter #(prime-of? % p) primes) (inc idx))))))
+
