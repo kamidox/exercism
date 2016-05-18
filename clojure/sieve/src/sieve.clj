21107d854d5ce4f1215a964159f1df86229e5c8a
Joey.Huang <kamidox@qq.com>
2016-05-18 22:50:32 +0800

    clojure sieve rename

--- clojure/sieve/src/sieve.clj
+++ clojure/sieve/src/sieve.clj
@@ -1,6 +1,6 @@
 (ns sieve)
 
-(defn- prime-of? [n p]
+(defn- divisible? [n p]
   (or (<= n p) (not (zero? (mod n p)))))
 
 (defn sieve
@@ -9,5 +9,5 @@
   (loop [primes (range 2 (inc n)) idx 0]
     (if (<= (count primes) idx) primes
       (let [p (nth primes idx)]
-        (recur (filter #(prime-of? % p) primes) (inc idx))))))
+        (recur (filter #(divisible? % p) primes) (inc idx))))))
 
