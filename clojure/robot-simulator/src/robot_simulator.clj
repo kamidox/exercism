cc8c4cf4d37c95522638f1378c76460f288b7a97
Joey.Huang <kamidox@qq.com>
2016-05-19 23:15:16 +0800

    clojure robot simulator

--- clojure/robot-simulator/src/robot_simulator.clj
+++ clojure/robot-simulator/src/robot_simulator.clj
@@ -13,3 +13,19 @@
 
 (defn turn-left [bearing] (turn bearing (reverse directions)))
 
+(defn move-forward [robbie]
+  (let [bearing (:bearing robbie)
+        coords (:coordinates robbie)
+        new-coords (cond (= bearing :north) (assoc coords :y (inc (:y coords)))
+                         (= bearing :east) (assoc coords :x (inc (:x coords)))
+                         (= bearing :west) (assoc coords :x (dec (:x coords)))
+                         (= bearing :south) (assoc coords :y (dec (:y coords))))]
+    (assoc robbie :coordinates new-coords)))
+
+(defn move-step [robbie step]
+  (cond (= step \R) (assoc robbie :bearing (turn-right (:bearing robbie)))
+        (= step \L) (assoc robbie :bearing (turn-left (:bearing robbie)))
+        (= step \A) (move-forward robbie)))
+
+(defn simulate [steps robbie]
+  (reduce move-step robbie steps))
