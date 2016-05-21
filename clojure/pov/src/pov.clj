b4e77aeb28183f4d793fbb8000d3e948f447fb02
Joey.Huang <kamidox@qq.com>
2016-05-21 23:21:18 +0800

    clojure pov

---
+++ clojure/pov/src/pov.clj
@@ -0,0 +1,47 @@
+(ns pov
+  (:require [clojure.zip :as zip]))
+
+; node => [parent [child-1] [child-2] ... [child-n]]
+; each child is a node
+; use clojure.zip to do the functional tree editing and manipulation.
+; refer: http://clojuredocs.org/clojure.zip
+
+(defn- remove-child
+  "Remove a child from a node"
+  [parent child]
+  (filter #(not= child %) parent))
+
+(defn- reparent
+  "Take a node, and make it the parent of its parent"
+  [loc]
+  (let [current (zip/node loc)
+        parent (zip/up loc)
+        belonged-to (when parent
+                      (-> parent
+                          (zip/edit remove-child current)
+                          reparent
+                          list))]
+    (vec (concat current belonged-to))))
+
+(defn- find-node
+  "Find the node whose identifier matches the given value"
+  [v tree]
+  (loop [loc (-> tree zip/vector-zip zip/next)]
+    (cond (= v (zip/node loc)) (zip/up loc)
+          (zip/end? loc) nil
+          :else (recur (zip/next loc)))))
+
+(defn of
+  "Find a node by its identifier and raise to the root"
+  [s tree]
+  (when-let [loc (find-node s tree)]
+    (reparent loc)))
+
+(defn path-from-to
+  "Find a path from src node to dest node"
+  [src dest graph]
+  (when-let [loc (find-node dest (of src graph))]
+    (let [dest-node (zip/node loc)
+          path (concat (zip/path loc) [dest-node])]
+      (map first path))))
+
