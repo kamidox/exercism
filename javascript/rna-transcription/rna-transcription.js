d81624127bf66dd2d3ec2a66b7b4f1f3c86bac3a
Joey.Huang <kamidox@qq.com>
2016-05-22 02:05:01 +0800

    javascript rna transcription

---
+++ javascript/rna-transcription/rna-transcription.js
@@ -0,0 +1,21 @@
+var DnaTranscriber = function () {};
+
+// Key Hint:
+// 1. use Json to map DNA to RNA
+// 2. use String.prototype.replace() to convert
+// 3. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace
+
+var dnaToRna = {
+  G: 'C',
+  C: 'G',
+  T: 'A',
+  A: 'U'
+};
+
+DnaTranscriber.prototype.toRna = function (dna) {
+  return dna.replace(/./g, function (match) {
+    return dnaToRna[match] || '';
+  });
+};
+
+module.exports = DnaTranscriber;
---
+++ javascript/rna-transcription/rna-transcription.spec.js
@@ -0,0 +1,34 @@
+var DnaTranscriber = require('./rna-transcription');
+var dnaTranscriber = new DnaTranscriber();
+
+describe('toRna()', function() {
+
+  it('transcribes cytosine to guanine', function() {
+    expect(dnaTranscriber.toRna('C')).toEqual('G');
+  });
+
+  it('transcribes guanine to cytosine', function() {
+    expect(dnaTranscriber.toRna('G')).toEqual('C');
+  });
+
+  it('transcribes adenine to uracil', function() {
+    expect(dnaTranscriber.toRna('A')).toEqual('U');
+  });
+
+  it('transcribes thymine to adenine', function() {
+    expect(dnaTranscriber.toRna('T')).toEqual('A');
+  });
+
+  it('transcribes all dna nucleotides to their rna complements', function() {
+    expect(dnaTranscriber.toRna('ACGTGGTCTTAA'))
+        .toEqual('UGCACCAGAAUU');
+  });
+
+  it("transcribes all invalid dna nucleotides", function () {
+    expect(dnaTranscriber.toRna('XXXX')).toEqual('');
+  });
+
+  it("transcribes partial invalid dna nucleotides", function () {
+    expect(dnaTranscriber.toRna('XCXGXAXTX')).toEqual('GCUA');
+  });
+});
