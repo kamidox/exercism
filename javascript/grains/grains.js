a50edb9b10e47c7a02b18a17b3e179388280de0e
Joey.Huang <kamidox@qq.com>
2016-06-01 22:03:01 +0800

    javascript grains

--- javascript/grains/grains.js
+++ javascript/grains/grains.js
@@ -4,8 +4,15 @@ function Grains () {
 }
 
 Grains.prototype.square = function (n) {
-  var bigI = new BigInt(n);
-  return bigI.times(n).toString();
+    return BigInt(2).pow(n-1).toString();
+}
+
+Grains.prototype.total = function () {
+    var result = new BigInt(0);
+    for (var i = 0; i < 64; i ++) {
+        result = result.add(BigInt(2).pow(i))
+    }
+    return result.toString();
 }
 
 module.exports = Grains;
