da6f34aebd4003787f7c268faf9b7c0dff64249c
Joey.Huang <kamidox@qq.com>
2016-06-02 21:22:30 +0800

    javascript acronym

--- javascript/acronym/acronym.js
+++ javascript/acronym/acronym.js
@@ -1,11 +1,31 @@
 function Acronyms () {
 }
 
-Acronyms.prototype.parse = function (phrases) {
+function concat(pValue, cValue, index, array) {
+    return pValue + cValue.toUpperCase();
+}
+
+Acronyms.parse = function (phrases) {
     var words = phrases.split(/\W/);
-    return words.map(function (currentValue, index, array) {
-        return currentValue[0].toUpperCase();
+    var acr = words.map(function (currentValue, index, array) {
+        if (currentValue[0]) {
+            if (currentValue.toUpperCase() == currentValue || currentValue.toLowerCase() == currentValue) {
+                return currentValue[0];
+            } else {
+                var subacr = currentValue.split('').filter(function (element, index, array) {
+                    if (element.search(/\w/) != -1 && element.toUpperCase() == element) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                });
+                return subacr.reduce(concat, '');
+            }
+        } else {
+            return '';
+        }
     });
+    return acr.reduce(concat, '');
 }
 
 module.exports = Acronyms;
