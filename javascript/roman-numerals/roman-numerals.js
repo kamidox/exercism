4b9e6fff452d310cb8453d470cb3541be1a0d103
Joey Huang <kamidox@gmail.com>
2016-06-03 16:59:07 +0800

    roman numerals DRAFT

---
+++ javascript/roman-numerals/roman-numerals.js
@@ -0,0 +1,14 @@
+function toRoman (n) {
+    'use strict';
+    
+    function toDigits (n) {
+        var digits = [];
+        for (var i = 1000; i > 0; i = i / 10) {
+
+        }
+    }
+
+}
+
+module.exports = toRoman;
+
