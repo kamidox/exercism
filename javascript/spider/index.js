e483a08910c3e89d9ad7a2c0efbe2538c0089a4a
Joey Huang <kamidox@gmail.com>
2016-07-07 15:58:39 +0800

    javascript spider: download ok

--- javascript/spider/index.js
+++ javascript/spider/index.js
@@ -1,7 +1,7 @@
 const spider = require('./spider');
 
-var url = 'http://kamidox.com/index.html';
-var fname = 'C:\\Users\\CNJOHUA10\\lab\\spider\\blog.html'
+var url = 'http://blog.kamidox.com';
+var fname = './tmp/index.html'
 spider.download(url, fname)
     .then((body) => console.log(`Download Successed. Size = ${body.length}`))
     .catch((err) => console.log(err));
