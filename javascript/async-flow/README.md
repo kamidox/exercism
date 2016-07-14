7b56673ab956071d2896993149cc5bdde584d46c
Joey.Huang <kamidox@qq.com>
2016-07-15 00:35:27 +0800

    javascript async flow: add readme and thunk demo code

---
+++ javascript/async-flow/README.md
@@ -0,0 +1,3 @@
+# Async flow control with Generator
+
+This is the prototype of async flow libraries like [co](https://npmjs.org/package/co) and [suspend](https://npmjs.org/package/suspend).
--- javascript/async-flow/index.js
+++ javascript/async-flow/index.js
@@ -1,10 +1,30 @@
 var fs = require('fs');
 var path = require('path');
 var asyncFlow = require('./async-flow').asyncFlow;
+var asyncFlowWithThunk = require('./async-flow').asyncFlowWithThunk;
 
 asyncFlow(function* (callback) {
     var fname = path.basename(__filename);
     var data = yield fs.readFile(fname, 'utf8', callback);
     yield fs.writeFile('clone_' + fname, data, callback);
     console.log(`cloned: ${fname}`);
+})
+
+function readFileThunk(fname, opts) {
+    return function (callback) {
+        return fs.readFile(fname, opts, callback);
+    }
+}
+
+function writeFileThunk(fname, data) {
+    return function (callback) {
+        return fs.writeFile(fname, data, callback);
+    }
+}
+
+asyncFlowWithThunk(function* () {
+    var fname = path.basename(__filename);
+    var data = yield readFileThunk(fname, 'utf8');
+    yield writeFileThunk('clone_thunk_' + fname, data);
+    console.log(`cloned with thunk: ${fname}`);
 })
