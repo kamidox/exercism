a13409c58dce8f3718e1ddc73c6f1106f03fa5f4
Joey.Huang <kamidox@qq.com>
2020-05-04 22:33:44 +0800

    go grains

---
+++ go/grains/grains.go
@@ -0,0 +1,25 @@
+package grains
+
+import (
+	"errors"
+)
+
+func Square(n int) (uint64, error) {
+	if n <= 0 || n >= 65 {
+		return 0, errors.New("invalid square number")
+	}
+	var ret uint64 = 1
+	for i := 1; i < n; i++ {
+		ret *= 2
+	}
+	return ret, nil
+}
+
+func Total() uint64 {
+	var sum uint64 = 0
+	for i := 1; i <= 64; i++ {
+		s, _ := Square(i)
+		sum += s
+	}
+	return sum
+}
