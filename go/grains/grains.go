cacb15f55409cc4f9b65e1e88e40cd04f45864c9
Joey.Huang <kamidox@qq.com>
2020-05-04 22:44:05 +0800

    go grains

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -2,12 +2,15 @@ package grains
 
 import (
 	"errors"
+	//"math"
 )
 
 func Square(n int) (uint64, error) {
 	if n <= 0 || n >= 65 {
 		return 0, errors.New("invalid square number")
 	}
+	// math version using float64, which has poor performance
+	// var ret uint64 = uint64(math.Pow(2, float64(n-1)))
 	var ret uint64 = 1
 	for i := 1; i < n; i++ {
 		ret *= 2
